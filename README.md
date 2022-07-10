77461a3ff4dffad6c4f45d46dcd757b8e4a170a6
sinderella <rawsec@gmail.com>
2022-07-10 16:26:10 +0100

    add unapply commands

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Bionic Reading plugin for logseq
 
-The plugin relies on [text-vide](https://github.com/Gumball12/text-vide) to transform the paragraph(s). This plugin only introduces 2 slash commands.
+The plugin relies on [text-vide](https://github.com/Gumball12/text-vide) to transform the paragraph(s). This plugin only introduces the following slash commands.
 
 - `Bionic Reading: Apply`, apply bionic reading only to the current block
+- `Bionic Reading: Unapply`, unapply current applied block
 - `Bionic Reading: Apply Recursively`, which includes children blocks
+- `Bionic Reading: Unapply Recursively`, unapply current block and its children
 
 Inspired by https://www.reddit.com/r/logseq/comments/utkrin/would_make_for_a_cool_plugin/
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
 import '@logseq/libs';
 import { textVide } from 'text-vide';
 
-const addTextVide = async (blockId: string, recursive: boolean) => {
+const addTextVide = async (
+  blockId: string,
+  apply: boolean,
+  recursive: boolean
+) => {
   const block = await logseq.Editor.getBlock(blockId, {
     includeChildren: true,
   });
   if (block === null) {
     return;
   }
-  const cleanedContent = block.content.replaceAll('**', '');
-  logseq.Editor.updateBlock(
-    block.uuid,
-    textVide(cleanedContent, { sep: ['**', '**'] })
-  );
+  let cleanedContent = block.content.replaceAll('**', '');
+  if (apply) cleanedContent = textVide(cleanedContent, { sep: ['**', '**'] });
+  logseq.Editor.updateBlock(block.uuid, cleanedContent);
 
   if (recursive && block.children !== undefined) {
     block.children.map((b) => {
       if ('uuid' in b) {
-        return addTextVide(b.uuid, true);
+        return addTextVide(b.uuid, apply, recursive);
       } else {
         for (const uuid of b) {
-          addTextVide(uuid, true);
+          addTextVide(uuid, apply, recursive);
         }
         return;
       }
@@ -30,13 +32,24 @@ const addTextVide = async (blockId: string, recursive: boolean) => {
 
 const main = async () => {
   logseq.Editor.registerSlashCommand('Bionic Reading: Apply', async (e) => {
-    await addTextVide(e.uuid, false);
+    await addTextVide(e.uuid, true, false);
   });
 
   logseq.Editor.registerSlashCommand(
     'Bionic Reading: Apply Recursively',
     async (e) => {
-      await addTextVide(e.uuid, true);
+      await addTextVide(e.uuid, true, true);
+    }
+  );
+
+  logseq.Editor.registerSlashCommand('Bionic Reading: Unapply', async (e) => {
+    await addTextVide(e.uuid, false, false);
+  });
+
+  logseq.Editor.registerSlashCommand(
+    'Bionic Reading: Unapply Recursively',
+    async (e) => {
+      await addTextVide(e.uuid, false, true);
     }
   );
 };
